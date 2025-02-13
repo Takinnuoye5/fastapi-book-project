fa9678cf87e023633e00a0b1381a60bf1e0cba0e
Takinnuoye5 <akinnuoyet@gmail.com>
2025-02-13 17:19:24 +0000

    Fix main.py syntax error (remove accidental 'invalid code' text)

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ app.add_middleware(
 
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
-
 @app.get("/healthcheck")
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
+
